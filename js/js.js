bfa3d34e4984f10b458fafc2e7de4a83cec0c84e
Hau Tran <tranleconghau772001@gmail.com>
2023-10-15 02:48:24 +0700

    update featured courses

--- js/js.js
+++ js/js.js
@@ -26,4 +26,15 @@ window.onload = () => {
             });
         });
     });
+
+    // const element = document.querySelector(".scrolling-wrapper");
+
+    // element.addEventListener('wheel', (event) => {
+    //     event.preventDefault();
+
+    //     element.scrollBy({
+    //         left: event.deltaY < 0 ? -30 : 30,
+
+    //     });
+    // });
 }
